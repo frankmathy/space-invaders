031fc08d45969b8e45b195eccf25f3d0549771f9
= <=>
2023-12-09 16:59:42 +0100

    Added projectiles

--- index.js
+++ index.js
@@ -44,17 +44,38 @@ class Player {
     }
 }
 
+class Projectile {
+    constructor({ position, velocity }) {
+        this.position = position;
+        this.velocity = velocity;
+        this.radius = 3;
+    }
+
+    draw() {
+        c.beginPath();
+        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+        c.fillStyle = 'red';
+        c.fill();
+        c.closePath();
+    }
+
+    update() {
+        this.draw();
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
+    }
+}
+
 const player = new Player();
+const projectiles = [];
+
 const keys = {
     a: {
         pressed: false
     },
     d: {
         pressed: false
-    },
-    space: {
-        pressed: false
-    },
+    }
 }
 
 function animate() {
@@ -62,6 +83,14 @@ function animate() {
     c.fillStyle = 'black';
     c.fillRect(0, 0, canvas.width, canvas.height);
     player.update();
+
+    projectiles.forEach((projectile, index) => {
+        if (projectile.position.y + projectile.radius <= 0) {
+            projectiles.splice(index, 1);
+        } else {
+            projectile.update();
+        }
+    })
 
     if (keys.a.pressed && player.position.x > 0) {
         player.velocity.x = -7;
@@ -86,6 +115,16 @@ addEventListener('keydown', ({ key }) => {
             keys.d.pressed = true;
             break;
         case ' ':
+            projectiles.push(new Projectile({
+                position: {
+                    x: player.position.x + player.width / 2,
+                    y: player.position.y
+                },
+                velocity: {
+                    x: 0,
+                    y: -10
+                }
+            }))
             break;
     }
 })
