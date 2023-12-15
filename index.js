ae8a7a1d1f4478ea9113cebbe09a9bf03236e9b3
= <=>
2023-12-15 13:41:42 +0100

    Prettier formatting

--- index.js
+++ index.js
@@ -1,261 +1,325 @@
-const canvas = document.querySelector('canvas');
-const c = canvas.getContext('2d');
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
 
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
 class Player {
-    constructor() {
+  constructor() {
+    this.velocity = {
+      x: 0,
+      y: 0,
+    };
 
-        this.velocity = {
-            x: 0,
-            y: 0
-        }
+    this.rotation = 0;
 
-        this.rotation = 0;
+    this.image = new Image();
+    this.image.src = "img/spaceship.png";
+    this.image.onload = () => {
+      const scale = 0.5;
+      this.width = 100 * scale;
+      this.height = 100 * scale;
+      this.position = {
+        x: canvas.width / 2 - this.width / 2,
+        y: canvas.height - this.height - 20,
+      };
+    };
+  }
 
-        this.image = new Image();
-        this.image.src = 'img/spaceship.png';
-        this.image.onload = () => {
-            const scale = 0.5;
-            this.width = 100 * scale;
-            this.height = 100 * scale;
-            this.position = {
-                x: canvas.width / 2 - this.width / 2,
-                y: canvas.height - this.height - 20
-            }
-        }
+  draw() {
+    c.save();
+    c.translate(player.position.x + player.width / 2, player.position.y) +
+      player.height / 2;
+    c.rotate(this.rotation);
+    c.translate(-player.position.x - player.width / 2, -player.position.y) -
+      player.height / 2;
+    c.drawImage(
+      this.image,
+      this.position.x,
+      this.position.y,
+      this.width,
+      this.height
+    );
+    c.restore();
+  }
+
+  update() {
+    if (this.image && this.position) {
+      this.draw();
+      this.position.x += this.velocity.x;
     }
-
-    draw() {
-        c.save();
-        c.translate(player.position.x + player.width / 2, player.position.y) + player.height / 2;
-        c.rotate(this.rotation);
-        c.translate(-player.position.x - player.width / 2, -player.position.y) - player.height / 2;
-        c.drawImage(this.image, this.position.x, this.position.y, this.width, this.height);
-        c.restore();
-    }
-
-    update() {
-        if (this.image && this.position) {
-            this.draw();
-            this.position.x += this.velocity.x;
-        }
-    }
+  }
 }
 
 class Projectile {
-    constructor({ position, velocity }) {
-        this.position = position;
-        this.velocity = velocity;
-        this.radius = 5;
-    }
+  constructor({ position, velocity }) {
+    this.position = position;
+    this.velocity = velocity;
+    this.radius = 5;
+  }
 
-    draw() {
-        c.beginPath();
-        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
-        c.fillStyle = 'red';
-        c.fill();
-        c.closePath();
-    }
+  draw() {
+    c.beginPath();
+    c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+    c.fillStyle = "red";
+    c.fill();
+    c.closePath();
+  }
 
-    update() {
-        this.draw();
-        this.position.x += this.velocity.x;
-        this.position.y += this.velocity.y;
-    }
+  update() {
+    this.draw();
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+  }
+}
+
+class InvaderProjectile {
+  constructor({ position, velocity }) {
+    this.position = position;
+    this.velocity = velocity;
+    this.width = 3;
+    this.height = 10;
+  }
+
+  draw() {
+    c.fillStyle = "white";
+    c.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+
+  update() {
+    this.draw();
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+  }
 }
 
 class Invader {
-    constructor({ position }) {
+  constructor({ position }) {
+    this.velocity = {
+      x: 0,
+      y: 0,
+    };
 
-        this.velocity = {
-            x: 0,
-            y: 0
-        }
+    this.image = new Image();
+    this.image.src = "img/invader.png";
+    this.image.onload = () => {
+      const scale = 0.5;
+      this.width = 100 * scale;
+      this.height = 100 * scale;
+      this.position = {
+        x: position.x,
+        y: position.y,
+      };
+    };
+  }
 
-        this.image = new Image();
-        this.image.src = 'img/invader.png';
-        this.image.onload = () => {
-            const scale = 0.5;
-            this.width = 100 * scale;
-            this.height = 100 * scale;
-            this.position = {
-                x: position.x,
-                y: position.y
-            }
-        }
+  draw() {
+    c.drawImage(
+      this.image,
+      this.position.x,
+      this.position.y,
+      this.width,
+      this.height
+    );
+  }
+
+  update({ velocity }) {
+    if (this.image && this.position) {
+      this.draw();
+      this.position.x += velocity.x;
+      this.position.y += velocity.y;
     }
+  }
 
-    draw() {
-        c.drawImage(this.image, this.position.x, this.position.y, this.width, this.height);
-    }
-
-    update({ velocity }) {
-        if (this.image && this.position) {
-            this.draw();
-            this.position.x += velocity.x;
-            this.position.y += velocity.y;
-        }
-    }
+  shoot() {
+    invaderProjectiles.push(
+      new InvaderProjectile({
+        position: {
+          x: this.position.x + this.width / 2,
+          y: this.position.y + this.height,
+        },
+        velocity: {
+          x: 0,
+          y: 5,
+        },
+      })
+    );
+  }
 }
 
 class Grid {
-    constructor() {
-        this.invaderSize = 50;
-        this.position = {
-            x: 0,
-            y: 0
-        }
-        this.velocity = {
-            x: 3,
-            y: 0
-        }
+  constructor() {
+    this.invaderSize = 50;
+    this.position = {
+      x: 0,
+      y: 0,
+    };
+    this.velocity = {
+      x: 3,
+      y: 0,
+    };
 
-        this.invaders = [];
+    this.invaders = [];
 
-        const rows = Math.floor(Math.random() * 5 + 2);
-        const columns = Math.floor(Math.random() * 10 + 5);
+    const rows = Math.floor(Math.random() * 5 + 2);
+    const columns = Math.floor(Math.random() * 10 + 5);
 
-        this.width = columns * this.invaderSize;
+    this.width = columns * this.invaderSize;
 
-        for (let x = 0; x < columns; x++) {
-            for (let y = 0; y < rows; y++) {
-                this.invaders.push(new Invader({
-                    position: {
-                        x: x * this.invaderSize,
-                        y: y * this.invaderSize
-                    }
-                }))
-            }
-        }
+    for (let x = 0; x < columns; x++) {
+      for (let y = 0; y < rows; y++) {
+        this.invaders.push(
+          new Invader({
+            position: {
+              x: x * this.invaderSize,
+              y: y * this.invaderSize,
+            },
+          })
+        );
+      }
     }
+  }
 
-    update() {
-        this.position.x += this.velocity.x;
-        this.position.y += this.velocity.y;
-        this.velocity.y = 0;
+  update() {
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+    this.velocity.y = 0;
 
-        if (this.position.x + this.width >= canvas.width || this.position.x <= 0) {
-            this.velocity.x = -this.velocity.x;
-            this.velocity.y = this.invaderSize;
-        }
+    if (this.position.x + this.width >= canvas.width || this.position.x <= 0) {
+      this.velocity.x = -this.velocity.x;
+      this.velocity.y = this.invaderSize;
     }
+  }
 }
 
 const player = new Player();
 const projectiles = [];
 const grids = [];
+const invaderProjectiles = [];
 
 const keys = {
-    a: {
-        pressed: false
-    },
-    d: {
-        pressed: false
-    }
-}
+  a: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+};
 
 let frames = -1;
-let randomInterval = (Math.random() * 500) + 500;
+let randomInterval = Math.random() * 500 + 500;
 
 function animate() {
-    requestAnimationFrame(animate);
-    c.fillStyle = 'black';
-    c.fillRect(0, 0, canvas.width, canvas.height);
-    player.update();
+  requestAnimationFrame(animate);
+  c.fillStyle = "black";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+  player.update();
 
-    projectiles.forEach((projectile, index) => {
-        if (projectile.position.y + projectile.radius <= 0) {
-            projectiles.splice(index, 1);
-        } else {
-            projectile.update();
-        }
-    })
-
-    grids.forEach(grid => {
-        grid.update();
-        grid.invaders.forEach((invader, i) => {
-            invader.update({ velocity: grid.velocity });
-            projectiles.forEach((projectile, j) => {
-                if (projectile.position.y - projectile.radius <= invader.position.y + invader.height &&
-                    projectile.position.y + projectile.radius >= invader.position.y &&
-                    projectile.position.x + projectile.radius >= invader.position.x &&
-                    projectile.position.x - projectile.radius <= invader.position.x + invader.width) {
-                    setTimeout(() => {
-                        const invaderFound = grid.invaders.find((i) => i === invader);
-                        const projectileFound = projectiles.find((p) => p === projectile);
-
-                        if (invaderFound && projectileFound) {
-                            grid.invaders.splice(i, 1);
-                            projectiles.splice(j, 1);
-
-                            if (grid.invaders.length > 0) {
-                                const firstInvader = grid.invaders[0];
-                                const lastInvader = grid.invaders[grid.invaders.length - 1];
-                                grid.width = lastInvader.position.x - firstInvader.position.x + lastInvader.width;
-                                grid.position.ax = firstInvader.position.x;
-                            }
-                        }
-                    }, 0)
-                }
-            })
-        })
-    })
-
-    if (keys.a.pressed && player.position.x > 0) {
-        player.velocity.x = -7;
-        player.rotation = -0.15;
-    } else if (keys.d.pressed && player.position.x <= canvas.width - player.width) {
-        player.velocity.x = 7;
-        player.rotation = 0.15;
+  projectiles.forEach((projectile, index) => {
+    if (projectile.position.y + projectile.radius <= 0) {
+      projectiles.splice(index, 1);
     } else {
-        player.velocity.x = 0;
-        player.rotation = 0;
+      projectile.update();
     }
+  });
 
-    if (frames < 0 || frames > randomInterval) {
-        console.log('Created new invaders at ' + frames);
-        frames = 0;
-        randomInterval = Math.floor((Math.random() * 500) + 500);
-        grids.push(new Grid());
-    }
+  grids.forEach((grid, gridIndex) => {
+    grid.update();
+    grid.invaders.forEach((invader, i) => {
+      invader.update({ velocity: grid.velocity });
+      projectiles.forEach((projectile, j) => {
+        if (
+          projectile.position.y - projectile.radius <=
+            invader.position.y + invader.height &&
+          projectile.position.y + projectile.radius >= invader.position.y &&
+          projectile.position.x + projectile.radius >= invader.position.x &&
+          projectile.position.x - projectile.radius <=
+            invader.position.x + invader.width
+        ) {
+          setTimeout(() => {
+            const invaderFound = grid.invaders.find((i) => i === invader);
+            const projectileFound = projectiles.find((p) => p === projectile);
 
-    frames++;
+            if (invaderFound && projectileFound) {
+              grid.invaders.splice(i, 1);
+              projectiles.splice(j, 1);
+
+              if (grid.invaders.length > 0) {
+                const firstInvader = grid.invaders[0];
+                const lastInvader = grid.invaders[grid.invaders.length - 1];
+                grid.width =
+                  lastInvader.position.x -
+                  firstInvader.position.x +
+                  lastInvader.width;
+                grid.position.ax = firstInvader.position.x;
+              } else {
+                grids.splice(gridIndex, 1);
+              }
+            }
+          }, 0);
+        }
+      });
+    });
+  });
+
+  if (keys.a.pressed && player.position.x > 0) {
+    player.velocity.x = -7;
+    player.rotation = -0.15;
+  } else if (
+    keys.d.pressed &&
+    player.position.x <= canvas.width - player.width
+  ) {
+    player.velocity.x = 7;
+    player.rotation = 0.15;
+  } else {
+    player.velocity.x = 0;
+    player.rotation = 0;
+  }
+
+  if (frames < 0 || frames > randomInterval) {
+    console.log("Created new invaders at " + frames);
+    frames = 0;
+    randomInterval = Math.floor(Math.random() * 500 + 500);
+    grids.push(new Grid());
+  }
+
+  frames++;
 }
 
 animate();
 
-addEventListener('keydown', ({ key }) => {
-    switch (key) {
-        case 'a':
-            keys.a.pressed = true;
-            break;
-        case 'd':
-            keys.d.pressed = true;
-            break;
-        case ' ':
-            projectiles.push(new Projectile({
-                position: {
-                    x: player.position.x + player.width / 2,
-                    y: player.position.y
-                },
-                velocity: {
-                    x: 0,
-                    y: -10
-                }
-            }))
-            break;
-    }
-})
+addEventListener("keydown", ({ key }) => {
+  switch (key) {
+    case "a":
+      keys.a.pressed = true;
+      break;
+    case "d":
+      keys.d.pressed = true;
+      break;
+    case " ":
+      projectiles.push(
+        new Projectile({
+          position: {
+            x: player.position.x + player.width / 2,
+            y: player.position.y,
+          },
+          velocity: {
+            x: 0,
+            y: -10,
+          },
+        })
+      );
+      break;
+  }
+});
 
-addEventListener('keyup', ({ key }) => {
-    switch (key) {
-        case 'a':
-            keys.a.pressed = false;
-            break;
-        case 'd':
-            keys.d.pressed = false;
-            break;
-    }
-})
+addEventListener("keyup", ({ key }) => {
+  switch (key) {
+    case "a":
+      keys.a.pressed = false;
+      break;
+    case "d":
+      keys.d.pressed = false;
+      break;
+  }
+});
